3f319daa730f45773bd2331c9c812a3ad8ccd8ba
Gracified2004 <izeerhunmwunsee@gmail.com>
2024-06-20 16:37:19 +0100

    add AppNavigator.tsx

---
+++ src/app/Grace/AppNavigator.tsx
@@ -0,0 +1,22 @@
+// src/app/grace/AppNavigator.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import VoteScreen from './VoteScreen';
+import { type RootStackParamList } from './types';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Vote" component={VoteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
