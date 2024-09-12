82c25359a6927a2503ab33b32f70fc4619161926
theblvckdev <nathancodes05@gmail.com>
2024-09-12 13:51:37 +0100

    code updates

---
+++ src/app/nathan/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, accusantium
+      enim magni veniam soluta harum voluptates quia error maiores in minima
+      sed. Nobis culpa optio cum inventore ut consectetur aut.
+    </div>
+  );
+};
+
+export default Homepage;
