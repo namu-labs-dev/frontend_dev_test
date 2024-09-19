687b3040a1527402252ab565a988c2467a97a502
gerald-x <geraldokolodc@gmail.com>
2024-09-19 12:33:36 +0100

    Added import for new svg

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -4,3 +4,4 @@ export { default as control } from "./control.svg";
 export { default as arrowLeft } from "./arrowLeft.svg";
 export { default as outLink } from "./outLink.svg";
 export { default as customIcon } from "./customIcon.svg";
+export { default as star } from "./star.svg";
