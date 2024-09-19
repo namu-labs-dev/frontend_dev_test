cee124b07b007503f537ae67a232d4c21f067cf9
gerald-x <geraldokolodc@gmail.com>
2024-09-19 15:52:08 +0100

    Minor style updates

--- src/components/Atoms/Jumbotron/JumbotronContent.tsx
+++ src/components/Atoms/Jumbotron/JumbotronContent.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const JumbotronContent = (props: Props) => {
   return (
     <div className='ml-3 mt-2 pl-3 pt-2'>
-      <h4 className='mb-4 text-2xl font-bold text-white'>{props.title}</h4>
+      <h4 className='mb-4 text-3xl font-semibold text-white'>{props.title}</h4>
       <div className='w-max rounded-full bg-[#2C2D30] p-3'>
         <SVGAtom iconName='star' width={45} height={45} />
       </div>
