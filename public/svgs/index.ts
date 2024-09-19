b8f3c188b399441a6193e485bda3836d02633bcd
gerald-x <geraldokolodc@gmail.com>
2024-09-20 00:30:59 +0100

    Imports for newly added icons

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -7,3 +7,7 @@ export { default as customIcon } from "./customIcon.svg";
 export { default as star } from "./star.svg";
 export { default as ethLogo } from "./ethLogo.svg";
 export { default as nvirLogo } from "./nvirLogo.svg";
+export { default as dollar } from "./dollar.svg";
+export { default as historyIcon } from "./historyIcon.svg";
+export { default as walletIcon } from "./walletIcon.svg";
+export { default as messageIcon } from "./messageIcon.svg";
