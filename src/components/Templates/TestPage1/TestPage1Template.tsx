703c7e97af38975e1f416e417a42599abdd8a89c
gerald-x <geraldokolodc@gmail.com>
2024-09-19 11:35:04 +0100

    Base code for footer module and footer module props

--- src/components/Templates/TestPage1/TestPage1Template.tsx
+++ src/components/Templates/TestPage1/TestPage1Template.tsx
@@ -1,10 +1,12 @@
 import { TestPage1HeaderModule } from "~/components/Modules/TestPage1/TestPage1HeaderModule";
+import { TestingPage1FooterModule } from "~/components/Modules/TestPage1/TestPage1FooterModule";
 import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import React from "react";
 
 type Props = {
   headerModuleProps: React.ComponentProps<typeof TestPage1HeaderModule>;
+  footerModuleProps: React.ComponentProps<typeof TestingPage1FooterModule>
 }
 
 export const TestPage1Template = (props: Props) => {
@@ -14,6 +16,10 @@ export const TestPage1Template = (props: Props) => {
         <TestPage1HeaderModule {...props.headerModuleProps} />
       </Header>
 
+
+      <Footer>
+        <TestingPage1FooterModule {...props.footerModuleProps} />
+      </Footer>
     </Layout>
   );
 };
