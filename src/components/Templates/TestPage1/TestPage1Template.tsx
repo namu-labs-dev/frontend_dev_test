5bc80337e51997fc231481b51b496257394fc58e
gerald-x <geraldokolodc@gmail.com>
2024-09-19 03:10:39 +0100

    Added TestPage1 Header Module components and props

--- src/components/Templates/TestPage1/TestPage1Template.tsx
+++ src/components/Templates/TestPage1/TestPage1Template.tsx
@@ -1,19 +1,19 @@
-import { PageWithToastHeaderModule } from "~/components/Modules/PageWithToast/PageWithToastHeaderModule";
+import { TestPage1HeaderModule } from "~/components/Modules/TestPage1/TestPage1HeaderModule";
 import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
+import React from "react";
 
 type Props = {
-  pageWithToastHeaderModuleProps: React.ComponentProps<
-    typeof PageWithToastHeaderModule
-  >;
-};
+  headerModuleProps: React.ComponentProps<typeof TestPage1HeaderModule>;
+}
 
-export const PageWithToastTemplate = (props: Props) => {
+export const TestPage1Template = (props: Props) => {
   return (
     <Layout style={{ height: "100%" }}>
       <Header style={{ padding: 0, height: 50 }}>
-        <PageWithToastHeaderModule {...props.pageWithToastHeaderModuleProps} />
+        <TestPage1HeaderModule {...props.headerModuleProps} />
       </Header>
+
     </Layout>
   );
 };
