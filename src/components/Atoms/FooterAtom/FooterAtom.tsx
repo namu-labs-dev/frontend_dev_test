27e37a043464ce9e19f58b98cb29e541038b5757
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-20 20:27:26 +0100

    add conditional rendering to footer

--- src/components/Atoms/FooterAtom/FooterAtom.tsx
+++ src/components/Atoms/FooterAtom/FooterAtom.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import SVGAtom from "../SVGAtom/SVGAtom";
 
 type Props = {
@@ -18,14 +19,23 @@ const icons: Array<{
 ];
 
 export const FooterAtom = ({ title }: Props) => {
+  const [walletClicked, setWalletClicked] = useState(false);
+
+  const handleIconClick = (iconName: string) => {
+    if (iconName === "wallet") {
+      setWalletClicked(!walletClicked);
+    }
+  };
+
   return (
     <div className='flex h-full items-center justify-between bg-[#1F1F1F] px-16 py-5 text-white'>
       {icons.map((icon, index) => (
         <div
           key={index}
-          className={`z-[1] flex w-[50px] cursor-pointer items-center justify-center ${icon.special ? "relative pt-2" : ""}`}
+          className={`z-[1] flex w-[50px] cursor-pointer items-center justify-center ${icon.special && walletClicked ? "relative pt-2" : ""}`}
+          onClick={() => handleIconClick(icon.iconName)}
         >
-          {icon.special && (
+          {icon.special && walletClicked && (
             <div className='absolute left-[-15px] right-[-15px] top-[-19px] h-[3px] bg-green-500'></div>
           )}
           <SVGAtom
