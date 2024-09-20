940f4ac132e3f15a688c6f57ae9062fded88f613
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-20 21:11:07 +0100

    add a settings component

--- src/components/Atoms/FooterAtom/FooterAtom.tsx
+++ src/components/Atoms/FooterAtom/FooterAtom.tsx
@@ -1,49 +1,28 @@
-import React, { useState } from "react";
 import SVGAtom from "../SVGAtom/SVGAtom";
 
 type Props = {
   title: string;
 };
 
-const icons: Array<{
-  iconName: "dollar" | "message" | "wallet" | "history";
-  width: number;
-  height: number;
-  color: string;
-  special?: boolean;
-}> = [
-  { iconName: "dollar", width: 30, height: 30, color: "white" },
-  { iconName: "message", width: 30, height: 30, color: "white" },
-  { iconName: "wallet", width: 30, height: 30, color: "white", special: true },
-  { iconName: "history", width: 30, height: 30, color: "white" },
+const icons: Array<{ iconName: 'dollar' | 'message' | 'wallet' | 'history', width: number, height: number, color: string, special?: boolean }> = [
+  { iconName: 'dollar', width: 30, height: 30, color: 'white' },
+  { iconName: 'message', width: 30, height: 30, color: 'white' },
+  { iconName: 'wallet', width: 30, height: 30, color: 'white', special: true },
+  { iconName: 'history', width: 30, height: 30, color: 'white' },
 ];
 
 export const FooterAtom = ({ title }: Props) => {
-  const [walletClicked, setWalletClicked] = useState(false);
-
-  const handleIconClick = (iconName: string) => {
-    if (iconName === "wallet") {
-      setWalletClicked(!walletClicked);
-    }
-  };
-
   return (
     <div className='flex h-full items-center justify-between bg-[#1F1F1F] px-16 py-5 text-white'>
       {icons.map((icon, index) => (
         <div
           key={index}
-          className={`z-[1] flex w-[50px] cursor-pointer items-center justify-center ${icon.special && walletClicked ? "relative pt-2" : ""}`}
-          onClick={() => handleIconClick(icon.iconName)}
+          className={`z-[1] flex w-[50px] cursor-pointer items-center justify-center ${icon.special ? 'relative pt-2' : ''}`}
         >
-          {icon.special && walletClicked && (
+          {icon.special && (
             <div className='absolute left-[-15px] right-[-15px] top-[-19px] h-[3px] bg-green-500'></div>
           )}
-          <SVGAtom
-            iconName={icon.iconName}
-            width={icon.width}
-            height={icon.height}
-            color={icon.color}
-          />
+          <SVGAtom iconName={icon.iconName} width={icon.width} height={icon.height} color={icon.color} />
         </div>
       ))}
       {/* <div>{title}</div> */}
--- src/components/Atoms/HeaderAtom/HeaderAtom.tsx
+++ src/components/Atoms/HeaderAtom/HeaderAtom.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { SettingOutlined } from "@ant-design/icons";
 import SVGAtom from "../SVGAtom/SVGAtom";
+import Image from "next/image";
 
 type Props = {
   title: string;
@@ -7,28 +9,57 @@ type Props = {
   onClickRightIcon?: () => void;
 };
 
-export const HeaderAtom = (props: Props) => {
+export const HeaderAtom = ({
+  title,
+  onClickLeftIcon,
+  onClickRightIcon,
+}: Props) => {
+  const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+
+  const handleSettingsClick = () => {
+    setIsSettingsVisible(!isSettingsVisible);
+  };
+
   return (
     <div className='relative flex h-full w-full items-center justify-center bg-[#1F1F1F] text-white'>
-      {/* Sample using custom svg */}
-      {props.onClickLeftIcon && (
+      {onClickLeftIcon && (
         <div
           className='absolute left-0 z-[1] flex w-[50px] cursor-pointer items-center justify-center'
-          onClick={props.onClickLeftIcon}
+          onClick={onClickLeftIcon}
         >
           <SVGAtom iconName='atomLeft' width={23} height={23} color='white' />
         </div>
       )}
 
-      <div>{props.title}</div>
+      <div>{title}</div>
 
-      {/* Sample using Antd icon */}
-      {props.onClickRightIcon && (
+      {onClickRightIcon && (
         <div
           className='absolute right-0 z-[1] flex w-[50px] cursor-pointer items-center justify-center'
-          onClick={() => console.log("Antd icon clicked")}
+          onClick={handleSettingsClick}
         >
           <SettingOutlined className='!text-gray-header cursor-pointer text-[20px]' />
+        </div>
+      )}
+
+      {isSettingsVisible && (
+        <div className='fixed inset-0 flex items-center justify-center'>
+          <div className='flex h-1/2 w-[450px] flex-col items-center justify-center rounded bg-[#2C2D30] p-4 text-white shadow-lg'>
+            <h1 className='mb-2 text-2xl font-semibold'>
+              Transaction <br /> Processing
+            </h1>
+            <Image
+              src='/images/shield.svg'
+              width={300}
+              height={80}
+              alt='Shield'
+            />
+            <p className='text-center'>
+              Uploading your transaction to the node
+            </p>
+            <p>Please wait for a moment...</p>
+            <p>This may take up to 2 minutes.</p>
+          </div>
         </div>
       )}
     </div>
