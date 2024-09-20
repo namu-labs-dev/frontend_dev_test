41d0c348eb6865772bedf35e659fa351a100b4ea
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-20 20:25:14 +0100

    Refactor FooterAtom component to use dynamic icons

--- src/components/Atoms/FooterAtom/FooterAtom.tsx
+++ src/components/Atoms/FooterAtom/FooterAtom.tsx
@@ -4,23 +4,39 @@ type Props = {
   title: string;
 };
 
-export const FooterAtom = (props: Props) => {
+const icons: Array<{
+  iconName: "dollar" | "message" | "wallet" | "history";
+  width: number;
+  height: number;
+  color: string;
+  special?: boolean;
+}> = [
+  { iconName: "dollar", width: 30, height: 30, color: "white" },
+  { iconName: "message", width: 30, height: 30, color: "white" },
+  { iconName: "wallet", width: 30, height: 30, color: "white", special: true },
+  { iconName: "history", width: 30, height: 30, color: "white" },
+];
+
+export const FooterAtom = ({ title }: Props) => {
   return (
     <div className='flex h-full items-center justify-between bg-[#1F1F1F] px-16 py-5 text-white'>
-      <div className='z-[1] flex w-[50px] cursor-pointer items-center justify-center'>
-        <SVGAtom iconName='dollar' width={30} height={30} color='white' />
-      </div>
-      <div className='z-[1] flex w-[50px] cursor-pointer items-center justify-center'>
-        <SVGAtom iconName='message' width={30} height={30} color='white' />
-      </div>
-      <div className='relative z-[1] flex w-[50px] cursor-pointer items-center justify-center pt-2'>
-        <div className='absolute left-[-15px] right-[-15px] top-[-19px] h-[3px] bg-green-500'></div>
-        <SVGAtom iconName='wallet' width={30} height={30} color='white' />
-      </div>
-      <div className='z-[1] flex w-[50px] cursor-pointer'>
-        <SVGAtom iconName='history' width={30} height={30} color='white' />
-      </div>
-      {/* <div>{props.title}</div> */}
+      {icons.map((icon, index) => (
+        <div
+          key={index}
+          className={`z-[1] flex w-[50px] cursor-pointer items-center justify-center ${icon.special ? "relative pt-2" : ""}`}
+        >
+          {icon.special && (
+            <div className='absolute left-[-15px] right-[-15px] top-[-19px] h-[3px] bg-green-500'></div>
+          )}
+          <SVGAtom
+            iconName={icon.iconName}
+            width={icon.width}
+            height={icon.height}
+            color={icon.color}
+          />
+        </div>
+      ))}
+      {/* <div>{title}</div> */}
     </div>
   );
 };
