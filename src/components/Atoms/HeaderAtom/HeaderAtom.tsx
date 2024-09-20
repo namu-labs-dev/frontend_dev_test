cde56d4dee33d96e6ee0783bdc93e9a8d5b5d2a6
gerald-x <geraldokolodc@gmail.com>
2024-09-20 06:01:35 +0100

    Added conditional modal

--- src/components/Atoms/HeaderAtom/HeaderAtom.tsx
+++ src/components/Atoms/HeaderAtom/HeaderAtom.tsx
@@ -1,4 +1,5 @@
 import { SettingOutlined } from "@ant-design/icons";
+import { SettingsModal } from "~/components/Components/SettingsModal/SettingsModal";
 import * as svgIcons from "public/svgs";
 import SVGAtom from "../SVGAtom/SVGAtom";
 
@@ -8,11 +9,12 @@ type Props = {
   iconName?: keyof typeof svgIcons;
   onClickLeftIcon?: () => void;
   onClickRightIcon?: () => void;
+  modalProps?: React.ComponentProps<typeof SettingsModal>;
 };
 
 export const HeaderAtom = (props: Props) => {
   // If darkTheme is true the background color becomes dark and the primary color
-  // for icons and font becomes white 
+  // for icons and font becomes white
   return (
     <div
       className={`relative flex h-full w-full items-center justify-center ${props.darkTheme ? "bg-[#1F1F1F]" : "bg-[#FFC96F]"}`}
@@ -23,19 +25,36 @@ export const HeaderAtom = (props: Props) => {
           className='absolute left-0 z-[1] flex w-[50px] cursor-pointer items-center justify-center'
           onClick={props.onClickLeftIcon}
         >
-          <SVGAtom iconName={`${props.iconName ? props.iconName : "arrowLeft"}`} width={20} height={20} color={`${props.darkTheme ? "white" : "black"}`} />
+          <SVGAtom
+            iconName={`${props.iconName ? props.iconName : "arrowLeft"}`}
+            width={20}
+            height={20}
+            color={`${props.darkTheme ? "white" : "black"}`}
+          />
         </div>
       )}
 
-      <div className={`${props.darkTheme ? "text-white" : "text-black"}`}>{props.title}</div>
+      <div className={`${props.darkTheme ? "text-white" : "text-black"}`}>
+        {props.title}
+      </div>
 
       {/* Sample using Antd icon */}
       {props.onClickRightIcon && (
         <div
           className='absolute right-0 z-[1] flex w-[50px] cursor-pointer items-center justify-center'
-          onClick={() => console.log("Antd icon clicked")}
+          onClick={() => {
+            if (props.modalProps) {
+              props.modalProps.modalProps.setModalOpen(true);
+            } else {
+              console.log("Antd icon clicked");
+            }
+          }}
         >
-          <SettingOutlined className={`${props.darkTheme ? "!text-white" : "!text-gray-header"} cursor-pointer text-[20px]`} />
+          <SettingOutlined
+            className={`${props.darkTheme ? "!text-white" : "!text-gray-header"} cursor-pointer text-[20px]`}
+          />
+
+          {props.modalProps && <SettingsModal {...props.modalProps} />}
         </div>
       )}
     </div>
