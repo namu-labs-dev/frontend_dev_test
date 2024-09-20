30a83d8a2f0e18d5027716d810b5c9e26abef152
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-20 23:34:41 +0100

    Refactor DanielFormContainer

--- src/app/daniel/testPage2/home/container/DanielFormContainer.tsx
+++ src/app/daniel/testPage2/home/container/DanielFormContainer.tsx
@@ -1,13 +1,17 @@
-"use client";
-import React from "react";
-import { PageWithFormContainer } from "~/app/sample/pageWithForm/container/PageWithFormContainer";
+"use client"; // Enable client-side rendering in a Next.js application
 
+import React from "react"; // Import React for JSX syntax and component creation
+import { PageWithFormContainer } from "~/app/sample/pageWithForm/container/PageWithFormContainer"; // Import the PageWithFormContainer component
+
+// Define a functional component called DanielFormContainer
 const DanielFormContainer = () => {
   return (
     <div>
+      {/* Render the PageWithFormContainer component */}
       <PageWithFormContainer />
     </div>
   );
 };
 
+// Export the DanielFormContainer as the default export from this module
 export default DanielFormContainer;
