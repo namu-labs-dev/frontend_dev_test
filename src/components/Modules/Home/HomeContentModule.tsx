7cdb1ce443031f0d4799edabd613bb394b9df19e
Temidayo32 <brainiedude@gmail.com>
2024-09-21 19:15:16 +0100

    Updated Home Content module

--- src/components/Modules/Home/HomeContentModule.tsx
+++ src/components/Modules/Home/HomeContentModule.tsx
@@ -1,13 +1,19 @@
 import { LinkList } from "~/components/Components/LinkList/LinkList";
 
 type Props = {
-  sampleLinks: string[];
+  title: string;
+  sampleLinks?: string[];
+  custom?: boolean;
 };
 
 export const HomeContentModule = (props: Props) => {
   return (
-    <div className="py-3">
-      <LinkList links={props.sampleLinks} />
+    <div className='py-3'>
+      <LinkList
+        links={props.sampleLinks}
+        title={props.title}
+        custom={props.custom}
+      />
     </div>
   );
 };
