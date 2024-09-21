6ce8aa5a0aa12f609716e5432bca7a847dcc7ad8
Temidayo32 <brainiedude@gmail.com>
2024-09-21 19:14:54 +0100

    Updated Home Footer Module

--- src/components/Modules/Home/HomeFooterModule.tsx
+++ src/components/Modules/Home/HomeFooterModule.tsx
@@ -1,9 +1,32 @@
 import { FooterAtom } from "~/components/Atoms/FooterAtom/FooterAtom";
 
 type Props = {
-  title: string;
+  title?: string;
+  /** Add custom prop to manage custom features as regards the task */
+  icons?: boolean;
+  bgColor?: string;
+  iconColor?: string;
+  handleWalletClick?: (icon: string) => void;
+  isClick?: string | null;
+  handleDollarClick?: (icon: string) => void;
+  handleMessageClick?: (icon: string) => void;
+  handleHistoryClick?: (icon: string) => void;
 };
 
 export const HomeFooterModule = (props: Props) => {
-  return <FooterAtom title={props.title} />;
+  return (
+    <div className='relative h-full'>
+      <FooterAtom
+        title={props.title}
+        icons={props.icons}
+        iconColor={props.iconColor}
+        bgColor={props.bgColor}
+        handleWalletClick={props.handleWalletClick}
+        isClick={props.isClick}
+        handleDollarClick={props.handleDollarClick}
+        handleMessageClick={props.handleMessageClick}
+        handleHistoryClick={props.handleHistoryClick}
+      />
+    </div>
+  );
 };
