7528f98bf09351b63a52f8f2c333b1e06ce31741
Temidayo32 <brainiedude@gmail.com>
2024-09-21 19:09:59 +0100

    Home Container

---
+++ src/app/temidayo/temidayo_testPage1/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import { HomeContainer } from "./container/HomeContainer";
+
+const Home = () => {
+  return <HomeContainer />;
+};
+
+export default Home;
