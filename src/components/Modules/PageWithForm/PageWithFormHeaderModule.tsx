9324680880457aec9746f705b0922c72c520bd7b
Temidayo32 <brainiedude@gmail.com>
2024-09-21 22:49:40 +0100

    added custom props

--- src/components/Modules/PageWithForm/PageWithFormHeaderModule.tsx
+++ src/components/Modules/PageWithForm/PageWithFormHeaderModule.tsx
@@ -2,11 +2,19 @@ import { HeaderAtom } from "~/components/Atoms/HeaderAtom/HeaderAtom";
 
 type Props = {
   title: string;
-  onClickLeftIcon: () => void;
+  // added custom props for the new features
+  weight?: string;
+  color?: string;
+  onClickLeftIcon?: () => void;
 };
 
 export const PageWithFormHeaderModule = (props: Props) => {
   return (
-    <HeaderAtom title={props.title} onClickLeftIcon={props.onClickLeftIcon} />
+    <HeaderAtom
+      title={props.title}
+      weight={props.weight}
+      onClickLeftIcon={props.onClickLeftIcon}
+      color={props.color}
+    />
   );
 };
