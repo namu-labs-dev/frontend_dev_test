224fc3764b1ada38d8219df27afb13d7db0bbe80
Ewoma Ozore <ewomaozore@gmail.com>
2024-09-25 12:11:57 +0100

    completion of testpage2

---
+++ src/app/ewoma/testPage2/container/TestPage2Container.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Form, Input, Button, Select, DatePicker, Upload, message } from 'antd';
+import { CameraFilled } from '@ant-design/icons';
+const { TextArea } = Input;
+
+export default function TestPage2Container() {
+    const [form] = Form.useForm();
+
+    const onFinish = (values: any) => {
+        message.success('Vote Created Successfully!');
+        console.log('Form Values:', values);
+    };
+
+    return (
+        <div className="min-h-screen">
+            <h1 className="text-xl font-bold text-center mb-4 pt-4">Vote</h1>
+
+            <div className="bg-[#F0F5FF] text-black px-7 py-4 mb-6">
+                <span className='font-bold text-[#2F54EB]'>10 FAO</span> will be deducted when voting is
+                generated (Amount: 9 FAO)
+            </div>
+
+            <div className='p-5'>
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onFinish={onFinish}
+                    className="max-w-lg mx-auto"
+                >
+                    <Form.Item
+                        label="Category"
+                        name="category"
+                        rules={[{ required: true, message: 'Please select a category!' }]}
+                    >
+                        <Select
+                            placeholder="Birthday AD"
+                            className="h-[54px]"
+                        >
+                            <Select.Option value="birthday">Birthday AD</Select.Option>
+                            <Select.Option value="wedding">Wedding AD</Select.Option>
+                            <Select.Option value="event">Event AD</Select.Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Vote Title"
+                        name="title"
+                        rules={[{ required: true, message: 'Please input the title!' }]}
+                    >
+                        <Input
+                            placeholder="Title"
+                            className="h-[54px]"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Image"
+                        name="image"
+                        rules={[{ required: true, message: 'Please upload an image!' }]}
+                    >
+                        <Upload
+                            listType="picture"
+                            beforeUpload={() => false}
+                        >
+                            <Button icon={<CameraFilled />} className="w-[458px] h-[136px]">
+                                {' '}
+                            </Button>
+                        </Upload>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Description"
+                        name="description"
+                        rules={[{ required: true, message: 'Please input the description!' }]}
+                    >
+                        <TextArea
+                            rows={4}
+                            placeholder="Please write down a description of the event"
+                            className="h-[136px]"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Start Date"
+                        name="startDate"
+                        rules={[{ required: true, message: 'Please select the start date!' }]}
+                    >
+                        <DatePicker
+                            className="w-full h-[54px]"
+                        />
+                    </Form.Item>
+
+                    <Form.Item className="text-center w-full">
+                        <Button type="primary" htmlType="submit" className='w-full h-[54px]'>
+                            Create Vote
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/ewoma/testPage2/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import TestPage2Container from "./container/TestPage2Container";
+
+const testPage2 = () => {
+    return <TestPage2Container />;
+};
+
+export default testPage2;
