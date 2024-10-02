81954b1625ad8c1bb9eb7c37dc46ea86d95cfa28
nelliejellie <emekaewelike@gmail.com>
2024-10-02 14:45:48 +0100

    renamed page title

---
+++ src/app/emeka/testPage1/container/TestPageContainer.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { EmptyTemplate } from "~/components/Templates/Empty/EmptyTemplate";
+
+export const EmptyContainer = () => {
+  const router = useRouter();
+
+  const emptyTemplateProps: React.ComponentProps<typeof EmptyTemplate> = {
+    emptyHeaderModuleProps: {
+      headerProps: {
+        title: "Test Page 1",
+        onClickLeftIcon: () => {
+          console.log("Left icon clicked");
+          router.back();
+        },
+        onClickRightIcon: () => console.log("Right icon clicked"),
+      },
+    },
+    emptyContentModuleProps: {
+      moduleProps: "Empty Content Module",
+      componentProps: { title: "EmptyContentComponent" },
+      atomProps: { title: "EmptyContentAtom" },
+    },
+    emptyFooterModuleProps: {
+      footerProps: { title: "EmptyFooterModule" },
+    },
+  };
+
+  return <EmptyTemplate {...emptyTemplateProps} />;
+};
---
+++ src/app/emeka/testPage1/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { EmptyContainer } from "./container/TestPageContainer";
+
+const Empty = () => {
+  return <EmptyContainer />;
+};
+
+export default Empty;
