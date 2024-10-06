a10270fea4e0b21e8619dcf5bc786255ab0f16e8
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-10-06 08:34:47 +0100

    Created screen content

--- src/components/Modules/TestPageOne/TestPageOneContentModule.tsx
+++ src/components/Modules/TestPageOne/TestPageOneContentModule.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { Message } from "~/app/ibuchukwu/testpage1/container/TestpageOneContainer";
+import { TestPageOneMessageComponent } from "~/components/Components/TestPageOneContentComponent/TestPageOneMessageComponent";
 
 type Props = {
   messages: Message[];
@@ -6,8 +8,26 @@ type Props = {
 
 export const TestPageOneContentModule = (props: Props) => {
   return (
-    <div className='flex flex-col gap-5'>
-      <h1>Messages</h1>
+    <div className='flex h-full flex-col'>
+      <div className='px-[20px]'>
+        <h1 className='font-inter text-[28px] font-[600] text-white'>
+          Messages
+        </h1>
+        <div className='mt-5 w-[65px]'>
+          <div className='flex h-[65px] items-center justify-center rounded-full bg-[#2C2D30]'>
+            <Image
+              src={"/assets/star.png"}
+              alt='star icon'
+              width={46}
+              height={46}
+            />
+          </div>
+          <p className='font-inter text-center text-[16px] text-white'>
+            Proxima
+          </p>
+        </div>
+      </div>
+      <TestPageOneMessageComponent messages={props.messages} />
     </div>
   );
 };
