d114c429648765f3eb428fdce353388c3f70c999
gerald-x <geraldokolodc@gmail.com>
2024-10-16 16:52:07 +0100

    Atom for success icon

---
+++ src/components/Atoms/IconAtom/SearchIconAtom.tsx
@@ -0,0 +1,9 @@
+import SVGAtom from "../SVGAtom/SVGAtom";
+
+export const SearchIconAtom = () => {
+  return (
+    <div className='h-[50px] w-[50px]'>
+      <SVGAtom iconName='searchIcon' />
+    </div>
+  );
+};
---
+++ src/components/Atoms/IconAtom/SuccessIconAtom.tsx
@@ -0,0 +1,9 @@
+import SVGAtom from "../SVGAtom/SVGAtom";
+
+export const SuccessIconAtom = () => {
+  return (
+    <div className='h-[50px] w-[50px]'>
+      <SVGAtom iconName='successIcon' />
+    </div>
+  );
+};
