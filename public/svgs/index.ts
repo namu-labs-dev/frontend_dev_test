d2d2beabe1d7016fb78e1ff7e3045c36f0cc484c
gerald-x <geraldokolodc@gmail.com>
2024-10-16 15:58:13 +0100

    Added latest icons import

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -4,3 +4,5 @@ export { default as control } from "./control.svg";
 export { default as arrowLeft } from "./arrowLeft.svg";
 export { default as outLink } from "./outLink.svg";
 export { default as animationIcon } from "./animationIcon.svg";
+export { default as successIcon } from "./successIcon.svg";
+export { default as searchIcon } from "./searchIcon.svg";
