c77f1cac26baf3984a391ee5a5d2f431cb1fef4b
DEE <donzydonathan@gmail.com>
2024-10-17 12:22:48 +0100

    chore: add svg icons

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -3,3 +3,7 @@ export { default as copy } from "./copy.svg";
 export { default as control } from "./control.svg";
 export { default as arrowLeft } from "./arrowLeft.svg";
 export { default as outLink } from "./outLink.svg";
+export { default as google } from "./google.svg";
+export { default as check } from "./check.svg";
+export { default as edit } from "./edit.svg";
+export { default as remove } from "./remove.svg";
