ac730411dbfa8f08ca2d9d79c71a3bce5eac0254
DEE <donzydonathan@gmail.com>
2024-10-17 10:11:18 +0100

    chore: setup boilerplate code for buidling and testing components

--- src/app/home/container/HomeContainer.tsx
+++ src/app/home/container/HomeContainer.tsx
@@ -2,16 +2,19 @@ import { message } from "antd";
 import useDummyCreatorData from "~/hooks/useDummyCreatorData";
 import useDummyNotificationData from "~/hooks/useDummyNotification";
 import useDummyQuestData from "~/hooks/useDummyQuestData";
-// import { HomeTemplate } from "~/components/Templates/Home/HomeTemplate";
+import { HomeTemplate } from "~/components/Templates/Home/HomeTemplate";
 
 export const HomeContainer = () => {
-
-  const creators = useDummyCreatorData()
-  const rankingQuests = useDummyQuestData('ranking')
-  const raffleQuests = useDummyQuestData('raffle');
+  const creators = useDummyCreatorData();
+  const rankingQuests = useDummyQuestData("ranking");
+  const raffleQuests = useDummyQuestData("raffle");
   const notifications = useDummyNotificationData();
 
+  const homeTemplateProps: React.ComponentProps<typeof HomeTemplate> = {
+    homeModuleProps: {
+      foo: "bar",
+    },
+  };
 
-  // return <HomeTemplate {...homeTemplateProps} />;
-  return <>hello </>
+  return <HomeTemplate {...homeTemplateProps} />;
 };
