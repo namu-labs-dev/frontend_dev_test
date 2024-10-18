2779446a6d90f2e421d31393e45a038856295b37
arkorede <israelashaolu2000@gmail.com>
2024-10-18 11:42:21 +0100

    feat: Created NFT Card Preview

--- src/components/Atoms/NFTCardAtom/NFTCard.tsx
+++ src/components/Atoms/NFTCardAtom/NFTCard.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Image from "next/image";
 import NFTImage from "../../../../public/pngs/NFTImage.png";
 import { StaticImageData } from "next/image";
+import NFTCardPreview from "./NFTCardPreview";
 
 type Props = {
   img?: StaticImageData;
@@ -9,8 +11,21 @@ type Props = {
 };
 
 export const NFTCard = (props: Props) => {
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
+
+  const handlePreview = () => {
+    setIsPreviewOpen(true);
+  };
+
+  const handleClosePreview = () => {
+    setIsPreviewOpen(false);
+  };
+
   return (
-    <div className='mx-auto w-full max-w-xs rounded-[0.625rem] border-2 border-[#000000] px-3.5 pb-5 pt-3.5'>
+    <div
+      className='mx-auto w-full max-w-xs cursor-pointer rounded-[0.625rem] border-2 border-[#000000] px-3.5 pb-5 pt-3.5'
+      onClick={handlePreview}
+    >
       <div className='relative w-full pb-[100%]'>
         <Image
           src={NFTImage}
@@ -21,9 +36,20 @@ export const NFTCard = (props: Props) => {
       </div>
 
       <div className='mt-3'>
-        <p className='text-lg font-medium leading-[1.6875rem]'>NFT Name</p>
-        <p className='text-base font-medium leading-6'>by Creator name</p>
+        <p className='text-lg font-medium leading-[1.6875rem]'>{props.name}</p>
+        <p className='text-base font-medium leading-6'>
+          by {props.creatorName}
+        </p>
       </div>
+
+      {isPreviewOpen && (
+        <NFTCardPreview
+          name='NFT Name'
+          imgUrl={NFTImage}
+          description='Description Description Description Description Description Description Description Description Description Description Description Description'
+          onClose={handleClosePreview}
+        />
+      )}
     </div>
   );
 };
