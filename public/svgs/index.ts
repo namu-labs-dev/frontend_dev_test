74279f7c6de06eec2f34c1a1674a658d01ac2212
DEE <donzydonathan@gmail.com>
2024-10-18 14:20:21 +0100

    feat: add and complete search dropdown

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -5,3 +5,4 @@ export { default as arrowLeft } from "./arrowLeft.svg";
 export { default as outLink } from "./outLink.svg";
 export { default as search } from "./search.svg";
 export { default as filter } from "./filter.svg";
+export { default as caret } from "./caret.svg";
