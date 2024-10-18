5f97150469f1ee8718f0a8998d88e4adee0c164b
DEE <donzydonathan@gmail.com>
2024-10-18 16:18:15 +0100

    feat: add and complete video progress component

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -3,3 +3,5 @@ export { default as copy } from "./copy.svg";
 export { default as control } from "./control.svg";
 export { default as arrowLeft } from "./arrowLeft.svg";
 export { default as outLink } from "./outLink.svg";
+export { default as pause } from "./pause.svg";
+export { default as play } from "./play.svg";
