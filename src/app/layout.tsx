e9489b449d4fff096cdb8286294185762f220441
gerald-x <geraldokolodc@gmail.com>
2024-10-21 16:32:09 +0100

    Removed class name causing error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className={`${poppins}`}>
+    <html lang='en' className={``}>
       <body>
         <div>
           <GlobalLayout>{children}</GlobalLayout>
