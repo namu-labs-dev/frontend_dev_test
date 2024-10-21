0656f7f15dc69ca914232e5fc516151d5478bc0d
arkorede <israelashaolu2000@gmail.com>
2024-10-21 23:35:49 +0100

    feat: updated hero module to align with the design and made it responsive

--- src/components/Components/HeroCarousel/HeroCarousel.tsx
+++ src/components/Components/HeroCarousel/HeroCarousel.tsx
@@ -27,40 +27,40 @@ export const HeroCarousel = (props: Props) => {
   };
 
   return (
-    <div className='relative w-full max-w-3xl'>
-      <div className='relative h-[27.875rem] w-[46.875rem] overflow-hidden rounded-lg md:h-96'>
+    <div className='relative mx-auto w-full max-w-[90vw] px-4 sm:max-w-2xl sm:px-6 md:max-w-3xl md:px-8'>
+      <div className='relative flex aspect-[1.385] w-full overflow-visible'>
         <Image
           src={images[currentIndex] as StaticImageData}
           alt={`Slide ${currentIndex + 1}`}
           layout='fill'
           objectFit='cover'
-          className='transition-opacity duration-500 ease-in-out'
+          className='rounded-lg transition-opacity duration-500 ease-in-out sm:rounded-[1.125rem]'
         />
 
         <button
           onClick={goToPrevious}
-          className='absolute left-4 top-1/2 -translate-y-1/2 rounded-full bg-white/30 p-2 text-white hover:bg-white/50 focus:outline-none'
+          className='absolute -left-3 top-1/2 z-10 -translate-y-1/2 rounded-lg border border-black bg-white/30 p-1 transition-all hover:bg-white/50 focus:outline-none sm:-left-8 sm:px-2 sm:py-1 md:-left-12'
         >
-          <ArrowLeftOutlined />
+          <ArrowLeftOutlined className='text-base sm:text-lg md:text-xl' />
         </button>
 
         <button
           onClick={goToNext}
-          className='absolute right-4 top-1/2 -translate-y-1/2 rounded-full bg-white/30 p-2 text-white hover:bg-white/50 focus:outline-none'
+          className='absolute -right-3 top-1/2 z-10 -translate-y-1/2 rounded-lg border border-black bg-white/30 p-1 transition-all hover:bg-white/50 focus:outline-none sm:-right-8 sm:px-2 sm:py-1 md:-right-12'
         >
-          <ArrowRightOutlined />
+          <ArrowRightOutlined className='text-base sm:text-lg md:text-xl' />
         </button>
       </div>
 
-      <div className='absolute bottom-4 left-1/2 flex -translate-x-1/2 transform space-x-2'>
+      <div className='absolute -bottom-[1.9375rem] left-[22%] flex -translate-x-1/2 transform space-x-2 sm:-bottom-14 sm:left-[28%] sm:space-x-3 md:-bottom-[3.6875rem]'>
         {images.map((_, slideIndex) => (
           <button
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className={`h-3 w-3 rounded-full transition-colors duration-300 ease-in-out focus:outline-none ${
+            className={`h-3 w-8 rounded-full transition-all duration-300 ease-in-out focus:outline-none sm:h-4 sm:w-[3.25rem] ${
               currentIndex === slideIndex
-                ? "bg-white"
-                : "bg-white/50 hover:bg-white/75"
+                ? "translate-x-[-2px] translate-y-[-2px] rounded-md bg-white shadow-[2px_2px_0px_black] sm:translate-x-[-4px] sm:translate-y-[-4px] sm:shadow-[4px_4px_0px_black]"
+                : "rounded-[3.125rem] border-2 border-black bg-white"
             }`}
           ></button>
         ))}
