bbf1192c5649bad764de6f50b700cf453b5a64a7
gerald-x <geraldokolodc@gmail.com>
2024-10-22 16:11:46 +0100

    Card layout for the error popup

---
+++ src/components/Components/CardLayout/HomeErrorCardLayout.tsx
@@ -0,0 +1,30 @@
+import CardLayout from "./CardLayout";
+import { FaCircleNotch } from "react-icons/fa";
+
+type Props = {
+  title: string;
+  message: string;
+  isModalOpen: boolean;
+  setModalOpen: (value: boolean) => void;
+  loadingAnimation?: boolean;
+};
+
+export const HomeErrorCardLayout = (props: Props) => {
+  return (
+    <CardLayout
+      isModalOpen={props.isModalOpen}
+      setModalOpen={props.setModalOpen}
+    >
+      <div className='ml-[2%] flex w-[80vw] flex-col justify-center gap-3 rounded-lg bg-white px-8 pb-6 pt-8 sm:w-[60vw] md:w-[45vw] lg:w-[35vw]'>
+        <h1 className='text-left text-2xl font-extrabold'>{props.title}</h1>
+        <div className='text-left text-gray-500'>{props.message}</div>
+
+        {props.loadingAnimation && (
+          <div className='mt-6'>
+            <FaCircleNotch className='animate-spin text-lg' />
+          </div>
+        )}
+      </div>
+    </CardLayout>
+  );
+};
