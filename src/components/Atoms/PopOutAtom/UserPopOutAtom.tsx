6f7136f6a5579ba7c1c7694dbd1f804ff4ab5e6e
gerald-x <geraldokolodc@gmail.com>
2024-10-22 16:10:18 +0100

    Added styles and changed avatar to user profile image

--- src/components/Atoms/PopOutAtom/UserPopOutAtom.tsx
+++ src/components/Atoms/PopOutAtom/UserPopOutAtom.tsx
@@ -1,10 +1,11 @@
-import { Card, Avatar } from "antd";
+import { Card } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import { LuMoveUpRight } from "react-icons/lu";
 import { VscCreditCard } from "react-icons/vsc";
 import { CiViewList } from "react-icons/ci";
 import { RiLogoutCircleRLine } from "react-icons/ri";
+import avatar from "../../../../public/pngs/avatar.png";
 
 export const UserPopOutAtom = () => {
   return (
@@ -16,27 +17,33 @@ export const UserPopOutAtom = () => {
       }}
     >
       <div className='flex items-center'>
-        <Avatar size={40} icon={<UserOutlined />} />
-        <div className='ml-4'>
+        <div className='mr-4 h-[40px] w-[40px] rounded-full'>
+          <img
+            className='h-full w-full object-cover'
+            src={avatar.src}
+            alt='user avatar png'
+          />
+        </div>
+        <div className=''>
           <div className='text-sm font-bold'>John Doe</div>
           <div className='truncate text-xs text-gray-500'>0x366476...768</div>
         </div>
         <LuMoveUpRight className='ml-auto text-gray-500' />
       </div>
 
-      <div className='mt-4 flex justify-between'>
+      <div className='mt-4 flex justify-between !font-medium'>
         <div className='text-sm'>Item Name</div>
         <div className='text-sm text-[#1838E0]'>999,999,999.45</div>
       </div>
 
       <div className='my-4 border-b-2 border-black' />
 
-      <div className='my-4 flex justify-between'>
-        <div className='text-sm'>Category</div>
+      <div className='mb-6 flex justify-between !font-medium'>
+        <div className='text-sm'>Purchased</div>
         <div className='text-sm text-[#1838E0]'>999,999,999.99</div>
       </div>
 
-      <div className='flex flex-col gap-y-2'>
+      <div className='flex flex-col gap-y-2 !font-medium'>
         <Link href='#' className='text-black'>
           <span className='mb-2 flex items-center'>
             <VscCreditCard className='mr-2' />
