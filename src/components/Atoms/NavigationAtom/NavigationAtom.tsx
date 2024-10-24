7065a65db6cb491206893aad20ed8c8d3b469785
arkorede <israelashaolu2000@gmail.com>
2024-10-24 13:45:17 +0100

    feat: modified classes to according to the tailwind config

--- src/components/Atoms/NavigationAtom/NavigationAtom.tsx
+++ src/components/Atoms/NavigationAtom/NavigationAtom.tsx
@@ -16,7 +16,7 @@ const NavigationAtom = (props: Props) => {
   const [isViewed, setIsViewed] = useState(true);
 
   return (
-    <nav className='border-b-2 border-[#000000] p-4'>
+    <nav className='border-b-2 border-black p-4'>
       <div className='flex h-16 items-center justify-between md:h-[5.875rem]'>
         <div className='flex items-center space-x-4'>
           <div className='h-8 w-32 md:h-[2rem] md:w-[9.5rem]'>
@@ -31,9 +31,9 @@ const NavigationAtom = (props: Props) => {
           <div className='hidden md:flex md:items-center md:space-x-4'>
             <a
               href='#'
-              className='rounded-[0.625rem] bg-[#000000] px-3 pb-[0.625rem] pt-[0.5625rem]'
+              className='rounded-common bg-black px-3 pb-[0.625rem] pt-[0.5625rem]'
             >
-              <p className='text-lg font-medium text-[#00FF00]'>Home</p>
+              <p className='text-lg font-medium text-primaryGreen'>Home</p>
             </a>
             <a
               href='#'
@@ -61,12 +61,14 @@ const NavigationAtom = (props: Props) => {
             </a>
           </div>
         </div>
-        <div className='flex items-center space-x-4'>
+        <div className='flex items-center justify-center space-x-4'>
           <a
             href='#'
-            className='hidden rounded-[0.625rem] bg-[#000000] px-5 py-1.5 md:block'
+            className='hidden rounded-common bg-black px-5 py-1.5 md:block'
           >
-            <p className='text-base font-medium text-[#00FF00]'>Create quest</p>
+            <p className='text-base font-medium text-primaryGreen'>
+              Create quest
+            </p>
           </a>
           {isLoggedIn ? (
             <>
@@ -77,17 +79,17 @@ const NavigationAtom = (props: Props) => {
                 placement='bottomRight'
                 arrow={false}
               >
-                <button className='inline-block text-2xl md:rounded-[0.625rem] md:!bg-[rgb(0,0,0)] md:!py-0.5 md:px-2'>
+                <button className='mb-1.5 inline-block text-2xl md:rounded-common md:!bg-black md:!py-0.5 md:px-2'>
                   {isViewed && (
-                    <div className='absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#00FF00] md:-right-[0.8125rem] md:-top-[0.625rem] md:h-[1.5625rem] md:w-[1.5625rem]'>
-                      <p className='text-xs font-medium text-[#000000] md:text-sm'>
+                    <div className='absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-primaryGreen md:-right-[0.8125rem] md:-top-[0.625rem] md:h-[1.5625rem] md:w-[1.5625rem]'>
+                      <p className='text-xs font-medium text-black md:text-sm'>
                         N
                       </p>
                     </div>
                   )}
                   <BellOutlined
-                    className='!relative  !rounded !p-0.5 !text-black hover:!bg-[#00FF00] hover:!text-black 
-                   md:!p-0 md:!text-white md:hover:!bg-transparent md:hover:!text-[#00FF00]'
+                    className='!relative !rounded !p-0.5 !text-black hover:!bg-primaryGreen 
+                   hover:!text-black md:!p-0 md:!text-white md:hover:!bg-transparent md:hover:!text-primaryGreen'
                     style={{ fontSize: "20px" }}
                   />
                 </button>
@@ -100,10 +102,14 @@ const NavigationAtom = (props: Props) => {
                 placement='bottomRight'
                 arrow={false}
               >
-                <Button className='inline-block text-2xl md:rounded-[0.625rem] md:!bg-[rgb(0,0,0)] md:!px-2 md:!py-0.5'>
+                <Button
+                  className='inline-block bg-white text-2xl md:rounded-common md:!bg-black md:!px-2 md:!py-0.5'
+                  style={{ backgroundColor: "transparent", border: "none" }}
+                  type='text'
+                >
                   <UserOutlined
-                    className='rounded p-0.5 text-black hover:bg-[#00FF00] hover:text-black 
-                   md:p-0 md:!text-white md:hover:!bg-transparent md:hover:!text-[#00FF00]'
+                    className='rounded p-0.5 text-black hover:!bg-primaryGreen hover:text-black 
+                   md:p-0 md:!text-white md:hover:!bg-transparent md:hover:!text-primaryGreen'
                     style={{ fontSize: "20px" }}
                   />
                 </Button>
@@ -112,14 +118,14 @@ const NavigationAtom = (props: Props) => {
           ) : (
             <a
               href='#'
-              className='hidden rounded-[0.625rem] bg-[#000000] px-5 py-1.5 md:inline-block'
+              className='hidden rounded-[0.625rem] bg-black px-5 py-1.5 md:inline-block'
             >
               <p className='text-base font-medium text-white'>Sign in</p>
             </a>
           )}
-          <button className='inline-block text-2xl md:hidden'>
+          <button className='mb-1.5 inline-block text-2xl md:hidden'>
             <MenuOutlined
-              className='rounded p-0.5 text-black hover:bg-[#00FF00] md:text-white'
+              className='rounded p-0.5 text-black hover:bg-primaryGreen md:text-white'
               style={{ fontSize: "20px" }}
             />
           </button>
